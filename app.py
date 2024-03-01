cbbdd5fcf7ece06d0fb0eda485dda292a58a7b60
cisco2053 <143852363+cisco2053@users.noreply.github.com>
2024-02-29 23:04:03 -0500

    primeros ejercicios python

---
+++ app.py
@@ -0,0 +1,40 @@
+#precioIVA = 0.19
+#precioProd = 100000
+#precioTotal = precioIVA * precioProd
+#print("El precio del iva es ", precioIVA, "Pesos")
+#print("El precio del producto es ", (precioIVA+precioProd))
+
+
+
+#n = int(input("Favor ingresa un numero"))
+#if n % 2 == 0:
+#   print("El numero ",n," es par")
+#else:
+#    print("El numero ",n," es impar")
+    
+    
+
+#for x in "clase de python":
+ #    print (x)"""
+     
+     
+
+#num = int(input("Contenido: 1(Ver numeros), 0(Salir) "))
+#while num != 0:    
+ #   x = 0
+    #while x < 10:        
+        #print("El numero x es: ",x)
+        #x += 1
+        
+        #print("Estas saliendo,,,")
+       # num = int(input("Contenido: 1(Ver numeros), 0(Salir) "))           
+    #print("Gracias.")
+    
+    
+    
+num = int(input("Favor escribe un numero positivo"))
+while num < 0:
+    print("Este es un numero negativo, prueba nuevamente")
+    num = int(input("Favor escribe un numero positivo"))
+print("El numero es: ", num)
+    
